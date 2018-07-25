e7c271c28c3f3c346410eee566ff95a608577694
befou2017 <benoit.fournier@sage.com>
2018-07-25 18:21:58 +0200

    Modificate Welcome MEssage

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,10 +68,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to New Community development of Longskate</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>Ce Site a dévellopement communautaire a pour but d'aider le monde du longskate ! </p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
